01894723e2b2cedf83378c521be2082262ee7293
Vladimir Alekseychuk <alekseychuk972mail.ru@gmail.com>
2019-09-27 21:28:31 +0300

    Updates

---
+++ precache-manifest.d89ee925d3c64a421cd7ad99350a085f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5932b58be3ca3af41098",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.59e15b71.chunk.css"
+  },
+  {
+    "revision": "5932b58be3ca3af41098",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.336e34e9.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "9e7d7636507c702e7ddd",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.1047985b.chunk.js"
+  },
+  {
+    "revision": "890b7918aec15e058a52bca3e9112661",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
